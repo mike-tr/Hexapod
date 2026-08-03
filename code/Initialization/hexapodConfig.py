12fbbd39e19ef21fe0088e1eb6801e2521fb9d74
Michael <48411662+mike-tr@users.noreply.github.com>
2026-08-03 20:23:24 +0300

    ik

--- code/Initialization/hexapodConfig.py
+++ code/Initialization/hexapodConfig.py
@@ -7,15 +7,20 @@ from pca9685 import PCA9685
 
 from leg import HexLeg
 from servo import Servo, ServoController
+from iksystem import IKSystem3
+
+COXA_LENGTH = 60.3
+FEMUR_LENGTH = 89.5
+TIBIA_LENGTH = 121.5
 
 
 class HexapodConfig:
     """Servo channel mapping and physical parameters for this hexapod."""
     def __init__(self):
         self.controller = ServoController()
+        self.iksys = IKSystem3(COXA_LENGTH, FEMUR_LENGTH, TIBIA_LENGTH)
         self.legR : list[HexLeg] = [HexLeg(self.controller,18,17,16), HexLeg(self.controller,21,20,19), HexLeg(self.controller,27,23,22)]
         self.legL : list[HexLeg] = [HexLeg(self.controller,13,14,15), HexLeg(self.controller,10,11,12), HexLeg(self.controller,31,8,9)]
-
 
     def relax(self):
         for leg in self.legR:
@@ -25,4 +30,4 @@ class HexapodConfig:
 
     # def moveLegAngle(self, right: bool, id: int, coxa_angle, femur_angle, tibia_angle):
     #     if right:
-    #         self.legR[id].se
+    #         pass
